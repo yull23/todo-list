8499ddabad6240dbcab55189d69d9967c719d2c5
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-11 16:47:47 -0500

    Adding basic design

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>Hello! World!</div>
+      <form action="">
+        <label htmlFor="item">New Item</label>
+        <input type="text" id="item" />
+        <button className="btn">Add Todo</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>
+          <input type="checkbox" />
+          <p>item#1</p>
+          <button className="btn">delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>item#2</p>
+          <button className="btn">delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>item#3</p>
+          <button className="btn">delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>item#4</p>
+          <button className="btn">delete</button>
+        </li>
+        <li>
+          <input type="checkbox" />
+          <p>item#5</p>
+          <button className="btn">delete</button>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
