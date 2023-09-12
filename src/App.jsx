8e39ba80e2d0dbcd0d5ade113a96b9aaff7b7196
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-09-12 03:59:10 -0500

    Complete pre-design of page functionality and styles.

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,25 @@ function App() {
       ...todos,
       { id: crypto.randomUUID(), title: newItem, completed: false },
     ]);
-    console.log(todos);
+    setNewItem("");
   }
 
+  function toggleTodo(id, checked) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: checked };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
   return (
     <div className="todo-list">
       <h1 className="todo-list__header">Todo List</h1>
@@ -35,14 +51,25 @@ function App() {
       <div className="todo-list__view">
         <h2 className="todo-list__subtitle">List current:</h2>
         <ul className="todo-list__list">
+          {todos.length == 0 && <p>{"No todos"}</p>}
           {todos.map((todo) => {
             return (
               <li className="todo-list__item" key={todo.id}>
                 <div className="todo-list__container-checkbox">
-                  <input type="checkbox" className="todo-list" />
+                  <input
+                    type="checkbox"
+                    className="todo-list"
+                    // checked={checked}
+                    onChange={(e) => {
+                      toggleTodo(todo.id, e.target.checked);
+                    }}
+                  />
                 </div>
                 <p className="todo-list__item-text">{todo.title}</p>
-                <button className="todo-list__item-button button">
+                <button
+                  className="todo-list__item-button button"
+                  onClick={(e) => deleteTodo(todo.id)}
+                >
                   delete
                 </button>
               </li>
